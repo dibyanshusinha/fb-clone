3a0f94e61bb8e8615b564e043d1ee554f90287e1
Dibyanshu Sinha <dibyanshu.sinha@deliverycentric.com>
2019-04-23 11:40:55 +0530

    Updates

---
+++ precache-manifest.4a51c09b664e5992a25b6ff3d0bcf785.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fdcf99f77002559fc11",
+    "url": "/fb-clone/static/js/runtime~main.22b09571.js"
+  },
+  {
+    "revision": "f5b927a4151c6c996dba",
+    "url": "/fb-clone/static/js/main.d1c31ef3.chunk.js"
+  },
+  {
+    "revision": "60e3d146f7a17732c42c",
+    "url": "/fb-clone/static/js/2.374bef66.chunk.js"
+  },
+  {
+    "revision": "f5b927a4151c6c996dba",
+    "url": "/fb-clone/static/css/main.4ce5dfc0.chunk.css"
+  },
+  {
+    "revision": "3450502a5b0053692b1fff68ac4f17e1",
+    "url": "/fb-clone/index.html"
+  }
+];
